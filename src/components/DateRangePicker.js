fb43ec16ed08c360d1f8893907918a5761be2050
Jason Gorst <jason.gorst@me.com>
2022-01-27 19:25:38 -0600

    add min and max dates.

--- src/components/DateRangePicker.js
+++ src/components/DateRangePicker.js
@@ -1,18 +1,22 @@
 import { Form } from 'semantic-ui-react'
 import { DatesRangeInput } from 'semantic-ui-calendar-react'
+import { MAX_DATE, MIN_DATE } from '../config'
 
 const DatesRangePicker = ({dateRange, onChange}) => {
   return (
     <Form.Field>
       <label>Date Range</label>
       <DatesRangeInput
-        allowSameEndDate
         name="dateRange"
         dateFormat="MM/DD/YYYY"
         icon="calendar alternate outline"
+        iconPosition="right"
+        allowSameEndDate
+        initialDate={MIN_DATE}
+        minDate={MIN_DATE}
+        maxDate={MAX_DATE}
         placeholder="Date From &ndash; Date To"
         value={dateRange}
-        iconPosition="right"
         onChange={onChange}
       />
     </Form.Field>
