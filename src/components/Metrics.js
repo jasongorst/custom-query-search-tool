b00b4241556d871b738541e1b08133eae82459ea
Jason Gorst <jason.gorst@me.com>
2022-01-29 16:51:10 -0600

    replace for loops with map/filter in Metric.js.

--- src/components/Metrics.js
+++ src/components/Metrics.js
@@ -5,38 +5,31 @@ const Metrics = ({metricDefinitions, metricOptions, metricCriteria, onChange}) =
   const [metricCount, setMetricCount] = useState(1)
 
   const handleMetricChange = (e, {metricIndex, name, value}) => {
-    let newMetricCriteria = []
-    for (let i = 0; i < metricCount; i++) {
-      newMetricCriteria.push((i === metricIndex) ? (
-        {...metricCriteria[i], [name]: value}
-      ) : (
-        metricCriteria[i]
-      ))
-    }
+    const newMetricCriteria = metricCriteria.map((criteria, index) => (
+      index === metricIndex ? {...criteria, [name]: value} : criteria
+    ))
+
     onChange(newMetricCriteria)
   }
 
   const addMetric = () => {
     setMetricCount(metricCount + 1)
-    onChange([
+
+    const newMetricCriteria = [
       ...metricCriteria,
       {
         metricCode: '',
         compareType: '',
         value: '',
       },
-    ])
+    ]
+    onChange(newMetricCriteria)
   }
 
   const dropMetric = (e, {metricIndex}) => {
     setMetricCount(metricCount - 1)
 
-    let newMetricCriteria = []
-    for (let i = 0; i < metricCount; i++) {
-      if (i !== metricIndex) {
-        newMetricCriteria.push(metricCriteria[i])
-      }
-    }
+    const newMetricCriteria = metricCriteria.filter((criteria, index) => index !== metricIndex)
     onChange(newMetricCriteria)
   }
 
