6b3a1c710b781679fad345a2f32fb9f8c6470984
Jason Gorst <jason.gorst@me.com>
2022-02-03 11:31:57 -0600

    hide Pagination if there's only 1 page. hide perPage Form.

--- src/components/DataPaginate.js
+++ src/components/DataPaginate.js
@@ -1,44 +1,46 @@
-import { Form, Grid, Pagination } from 'semantic-ui-react'
+import { Grid, Pagination } from 'semantic-ui-react'
 
 const DataPaginate = ({rows, page, perPage, handlePageChange, handlePerPageChange}) => {
-  const perPageValues = [25, 50, 100]
-  const perPageOptions = perPageValues.map((value) => (
-    {
-      key: value,
-      text: value.toString(),
-      value: value,
-    }
-  ))
+  // const perPageValues = [25, 50, 100]
+  // const perPageOptions = perPageValues.map((value) => (
+  //   {
+  //     key: value,
+  //     text: value.toString(),
+  //     value: value,
+  //   }
+  // ))
 
   return (
-    <Grid style={{paddingBottom: "1 rem"}}>
-      <Grid.Column width={13}>
-        <br/>
-        <Pagination
-          boundaryRange={1}
-          siblingRange={2}
-          firstItem={null}
-          lastItem={null}
-          totalPages={Math.ceil(rows / perPage)}
-          activePage={page}
-          onPageChange={handlePageChange}
-        />
-      </Grid.Column>
-      <Grid.Column width={3}>
-        <Form>
-          <Form.Field>
-            <Form.Select
-              name="perPage"
-              label="Items Per Page"
-              fluid
-              placeholder="Items Per Page&hellip;"
-              value={perPage}
-              options={perPageOptions}
-              onChange={handlePerPageChange}
-            />
-          </Form.Field>
-        </Form>
-      </Grid.Column>
+    <Grid>
+      <Grid.Row style={{paddingBottom: "1 rem"}}>
+        <Grid.Column width={13}>
+          <br/>
+          {rows > perPage && <Pagination
+            boundaryRange={1}
+            siblingRange={2}
+            firstItem={null}
+            lastItem={null}
+            totalPages={Math.ceil(rows / perPage)}
+            activePage={page}
+            onPageChange={handlePageChange}
+          />}
+        </Grid.Column>
+        <Grid.Column width={3}>
+          {/*<Form>*/}
+          {/*  <Form.Field>*/}
+          {/*    <Form.Select*/}
+          {/*      name="perPage"*/}
+          {/*      label="Items Per Page"*/}
+          {/*      fluid*/}
+          {/*      placeholder="Items Per Page&hellip;"*/}
+          {/*      value={perPage}*/}
+          {/*      options={perPageOptions}*/}
+          {/*      onChange={handlePerPageChange}*/}
+          {/*    />*/}
+          {/*  </Form.Field>*/}
+          {/*</Form>*/}
+        </Grid.Column>
+      </Grid.Row>
     </Grid>
   )
 }
