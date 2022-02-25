8859d36d9eb5cf147f7b24bd48ae6d20b9f91f9c
Jason Gorst <jason.gorst@me.com>
2022-02-25 16:15:34 -0600

    use "restaurantIds.length === 0" to check for empty restaurantIds array.

--- src/helpers/isFormComplete.js
+++ src/helpers/isFormComplete.js
@@ -4,7 +4,9 @@ import { DATE_FORMAT } from '../config'
 const isFormComplete = (formData) => {
   const [fromDate, toDate] = formData.dateRange.split(' - ').map((date) => moment(date, DATE_FORMAT).toISOString())
 
-  if (formData.restaurantIds === [] || fromDate === null || toDate === null ||
+  console.log(formData)
+
+  if (formData.restaurantIds.length === 0 || fromDate === null || toDate === null ||
     formData.fromHour === "" || formData.toHour === ""
   ) {
     return false
