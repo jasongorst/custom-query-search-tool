120995ceda94cabd57a6184afe4499a829b716bd
Jason Gorst <jason.gorst@me.com>
2022-02-26 19:17:23 -0600

    switch back end to my api.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 import moment from 'moment'
 
-export const URL_API = "https://customsearchquerytoolapi.azurewebsites.net"
+// export const URL_API = "https://customsearchquerytoolapi.azurewebsites.net"
+export const URL_API = "https://cqstapi.jasongorst.com"
 
 export const MIN_DATE = moment("2021-10-01")
 export const MAX_DATE = moment("2021-10-26")
